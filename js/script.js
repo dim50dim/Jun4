989624829718f41a5e820f191bb88ca4cf994095
dim50dim <zheliezkov@gmail.com>
2021-05-13 21:10:12 +0300

    first task

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+                          // First Task 
+             
+let result = +prompt('put a number here', '');
+let result1 = +prompt('See what"s happen', 1);
+
+function multi(a, b){
+    if(Number.isInteger(a, b)){
+        return alert(a ** b);
+    }else{
+        return alert('dude you are wrong');
+    }
+}
+multi(result, result1);
+
+               //SECOND TASK
+let str = '';
+let num = 0;
+let strOne = null;
+let bool = null;
+
+function second(a, b, c, d){
+    
+}
